a0ce051a86f3968c2fec0365712f198b1e242238
kushalnerpagar <kushalnerpagar@gmail.com>
2026-04-28 22:14:01 +0530

    add:login added along with JWT generation

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -1,4 +1,5 @@
 import prisma from "../config/db.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
@@ -25,6 +26,38 @@ export const signup = async (req, res) => {
     res.status(201).json({
       message: "User created successfully",
       user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      "secretkey", 
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
     });
   } catch (error) {
     console.log(error);
