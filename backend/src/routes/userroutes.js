1448f682087b176c1a046379730ff5d6beea210c
kushalnerpagar <kushalnerpagar@gmail.com>
2026-04-29 23:59:09 +0530

    update:user data is fetched from db instead of decrypted token

--- backend/src/routes/userroutes.js
+++ backend/src/routes/userroutes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import authMiddleware from "../middleware/authmiddleware.js";
+import prisma from "../config/db.js";
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, (req, res) => {
-  res.json({
-    message: "Protected route accessed",
-    user: req.user,
+router.get("/profile", authMiddleware, async (req, res) => {
+ const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.userId,
+    },
   });
+
+  res.json(user);
 });
 
 export default router;
