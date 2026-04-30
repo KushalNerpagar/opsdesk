d4c605fcd2e3d370954b19a3ef5473914e99ac03
kushalnerpagar <kushalnerpagar@gmail.com>
2026-04-30 21:36:34 +0530

    update:quick change of msg for member roles

--- backend/src/middleware/rolemiddleware.js
+++ backend/src/middleware/rolemiddleware.js
@@ -1,7 +1,7 @@
 const allowRoles = (...allowed) => {
   return (req, res, next) => {
     if (!req.user || !allowed.includes(req.user.role)) {
-      return res.status(403).json({ message: "Forbidden: insufficient role" });
+      return res.status(403).json({ message: "Forbidden: You don't have access to this resource" });
     }
     next();
   };
