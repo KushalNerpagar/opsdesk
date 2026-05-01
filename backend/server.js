341a7e676bc8b0564f7eb94e8f821a75a4c6d913
kushalnerpagar <kushalnerpagar@gmail.com>
2026-05-01 20:22:51 +0530

    update:project route connected to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./src/routes/authroutes.js";
 import userRoutes from "./src/routes/userroutes.js";
 import adminRoutes from "./src/routes/adminroutes.js";
+import projectRoutes from "./src/routes/projectroutes.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/projects", projectRoutes);
 
 app.listen(5000, () => {
     console.log("server running on port 5000 sucessfully");
